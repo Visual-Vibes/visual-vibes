759fcc4c64e36a3d2f3c6e0b640193e40e23d01d
Elliot Dubuque <edubuque13@gmail.com>
2023-12-13 13:08:55 -0500

    added supabase client creation

--- app/api/generate/route.tsx
+++ app/api/generate/route.tsx
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
 
 export const dynamic = "force-dynamic";
 
@@ -6,4 +7,9 @@ export async function POST(req: Request) {
   // Parse the request body
   const requestBody = await req.json();
   const email = requestBody.email;
+
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_SERVER_ROLE_KEY as string
+  );
 }
