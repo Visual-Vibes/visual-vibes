1f4b09afc9d4b770cd8714e4022b0ca5ed1a889a
bommarlu <bommarlu@umich.edu>
2023-12-16 17:53:04 -0800

    moved clock to center and added some styling

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
   const [hourCount, setHourCount] = useState(6);
 
   return (
-    <div className="flex flex-row items-center">
-      <Clock hourCount={hourCount} setHourCount={setHourCount} />
-      <div className="text-white ml-20 mr-auto">{hourCount}</div>
-    </div>
+<div className="flex flex-row justify-center items-center">
+  <Clock hourCount={hourCount} setHourCount={setHourCount} />
+  {/* <div className="text-white ml-20">{hourCount}</div> */}
+</div>
   );
 }
