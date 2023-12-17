12494a3df2f2c3396ebf4b234628b6f0bf74339e
bommarlu <bommarlu@umich.edu>
2023-12-17 14:21:35 -0800

    fixed initial ssr rendering issue in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,21 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaHome } from "react-icons/fa";
 import { useState, useEffect } from "react";
+import dynamic from 'next/dynamic'; // Import dynamic from next/dynamic
 
 // Custom Components
 import Clock from "@/components/Clock";
-import WelcomeImage from "@/components/WelcomeImage";
 
-import FloatingBackground from "@/components/FloatingBackground/FloatingBackground";
+// Use dynamic imports for FloatingBackground and WelcomeImage
+const FloatingBackground = dynamic(
+  () => import('@/components/FloatingBackground/FloatingBackground'),
+  { ssr: false }
+);
+
+const WelcomeImage = dynamic(
+  () => import('@/components/WelcomeImage'),
+  { ssr: false }
+);
 
 export default function Home() {
   const [hourCount, setHourCount] = useState(6);
